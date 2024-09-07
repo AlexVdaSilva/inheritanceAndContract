c3c0af6a71285310ca98d337b6ea6846c2d18d20
Alexandre Silva <alexandreviniciusdasilva@gmail.com>
2024-09-07 14:19:55 -0300

    Inheritance and contract, abstract class and interface

--- src/application/program.java
+++ src/application/program.java
@@ -2,9 +2,9 @@ package application;
 
 import java.util.Locale;
 
+import model.entities.AbstractShape;
 import model.entities.Circle;
 import model.entities.Rectangle;
-import model.entities.Shape;
 import model.enums.Color;
 
 public class program {
@@ -13,8 +13,8 @@ public class program {
 		
 		Locale.setDefault(Locale.US);
 		
-		Shape s1 = new Circle(Color.BLACK, 2.0);
-		Shape s2 = new Rectangle(Color.WHITE, 3.0, 4.0);
+		AbstractShape s1 = new Circle(Color.BLACK, 2.0);
+		AbstractShape s2 = new Rectangle(Color.WHITE, 3.0, 4.0);
 		
 		System.out.println("Circle color: " + s1.getColor());
 		System.out.println("Circle area: " + String.format("%.3f", s1.area()));
